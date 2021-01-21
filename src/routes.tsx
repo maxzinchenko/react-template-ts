5d0fe331a64961d06d194dac0c34cc10811de168
zzeenj <zzeenj@gmail.com>
2021-01-21 17:40:57 +0200

    add react version to view

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, { FC, version } from 'react';
 import { Route, BrowserRouter } from 'react-router-dom';
 
 export const Routes: FC = () => (
@@ -6,7 +6,7 @@ export const Routes: FC = () => (
     <Route path="/">
       { /* TODO remove this part of code */ }
       <div className="hello-world__wrapper">
-        <h1 className="hello-world__headline">Hello World</h1>
+        <h1 className="hello-world__headline">{ `React.js v ${ version }` }</h1>
       </div>
     </Route>
   </BrowserRouter>
